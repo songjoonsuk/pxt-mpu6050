eecef8f9c8bdc8ed720e96d76ecca4c3ab604235
Joonsuk Song <joonsuk0208@gmail.com>
2020-11-10 17:43:31 +0900

    test get data 1

--- mpu6050.ts
+++ mpu6050.ts
@@ -239,16 +239,16 @@ namespace MPU6050 {
     //% blockId="getMotion" block="Read Motion Data"
     export function getMotion6() {
 
-        RegisterHelper.readRegister8N(MPU6050_DEFAULT_ADDRESS,MPU6050_RA_ACCEL_XOUT_H, 14);
+        // RegisterHelper.readRegister8N(MPU6050_DEFAULT_ADDRESS,MPU6050_RA_ACCEL_XOUT_H, 14);
 
-/*
+
         ax = (gbuf[0] << 8) | gbuf[1] ;
         ay = (gbuf[2] << 8) | gbuf[3] ;
         az = (gbuf[4] << 8) | gbuf[5] ;
         gx = (gbuf[8] << 8) | gbuf[9] ;
         gy = (gbuf[10] << 8) | gbuf[11] ;
         gz = (gbuf[12] << 8) | gbuf[13] ;
-*/
+
     }   
 
     //% blockId="ReadAX" block="Read AX"
